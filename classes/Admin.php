1422cf9c0d440983660ac6a2f66665a6caa54521
abdelmoudiri <abdeljabbarmoudiri17@gmail.com>
2025-01-01 00:53:39 +0100

    ajouter une fonction a la index

--- classes/Admin.php
+++ classes/Admin.php
@@ -28,6 +28,85 @@ class Admin extends User{
             echo "Erreur : " . $e->getMessage();
         }
     }
+    public function createCategorie($nomCategorie)
+    {
+        require_once __DIR__ . "/database.php";
+        $requet = "INSERT INTO Categorie (nom) VALUES (:nom)";
+        
+        try {
+            $stmt = $conn->prepare($requet);
+            $stmt->bindParam(':nom', $nomCategorie, PDO::PARAM_STR);
+    
+            $stmt->execute();
+    
+            echo "Catégorie créée avec succès.";
+        } catch (PDOException $e) {
+            die("Erreur : " . $e->getMessage());
+        }
+    }
+    public function deleteCategorie($id_cat)
+    {
+        $id=intval($id_cat);
+        require_once __DIR__ . "/database.php";
+        $requet = "DELETE from Categorie where id_categorie= :id_categorie ";
+        
+        try {
+            $stmt = $conn->prepare($requet);
+            $stmt->bindParam(':id_categorie', $id, PDO::PARAM_INT);
+            $stmt->execute();
+    
+            echo "Catégorie suprimer avec succès.";
+        } catch (PDOException $e) {
+            die("Erreur : " . $e->getMessage());
+        }
+    }
+    public function accepterArticle($id_article)
+    {
+        $id = intval($id_article);
+        require_once __DIR__ . "/database.php";
+    
+        $requet = "UPDATE Article SET status = 'accepter' WHERE id_article = :id_article";
+    
+        try {
+            $stmt = $conn->prepare($requet);
+    
+            $stmt->bindParam(':id_article', $id, PDO::PARAM_INT);
+            $stmt->execute();
+
+            if ($stmt->rowCount() > 0) {
+                echo "Article accepté avec succès.";
+            } else {
+                echo "Aucun article trouvé avec cet ID.";
+            }
+        } catch (PDOException $e) {
+            die("Erreur : " . $e->getMessage());
+        }
+    }
+    public function refuserArticle($id_article)
+    {
+        $id = intval($id_article);
+        require_once __DIR__ . "/database.php";
+    
+        $requet = "UPDATE Article SET status = 'refuser' WHERE id_article = :id_article";
+    
+        try {
+            $stmt = $conn->prepare($requet);
+    
+            $stmt->bindParam(':id_article', $id, PDO::PARAM_INT);
+            $stmt->execute();
+
+            if ($stmt->rowCount() > 0) {
+                echo "Article refuser avec succès.";
+            } else {
+                echo "Aucun article trouvé avec cet ID.";
+            }
+        } catch (PDOException $e) {
+            die("Erreur : " . $e->getMessage());
+        }
+    }
+    
+
+    
     
 }
 
--- public/index.php
+++ public/index.php
@@ -1,10 +1,14 @@
 <?php
+include_once __DIR__ . "/classes/User.php";
+include_once __DIR__ . "/classes/Admin.php";
+
 if (isset($_GET['action'])) {
     $action = $_GET['action'];
 
     switch ($action) {
         case 'login':
-            include_once __DIR__ . "/views/login.php";
+
+            include_once __DIR__ . "/views/Admin.php";
             break;
         case 'register':
             include_once __DIR__ . "/views/login.php";
