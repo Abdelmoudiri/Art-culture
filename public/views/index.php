7bc7493dd3462b66d374ce55d1b4fdfedd3057e6
abdelmoudiri <abdeljabbarmoudiri17@gmail.com>
2025-01-01 13:03:23 +0100

    ajouter la finction du index.pphp pour afficher le dashboard du admin

--- public/views/index.php
+++ public/views/index.php
@@ -1,14 +1,15 @@
 <?php
-include_once __DIR__ . "/classes/User.php";
-include_once __DIR__ . "/classes/Admin.php";
+include_once   '/../classes/User.php';
+include_once   './../classes/Admin.php';
+echo __DIR__ ;
 
 if (isset($_GET['action'])) {
     $action = $_GET['action'];
 
     switch ($action) {
         case 'login':
-
-            include_once __DIR__ . "/views/Admin.php";
+            
+            include_once __DIR__ . "/../views/Admin.php";
             break;
         case 'register':
             include_once __DIR__ . "/views/login.php";
